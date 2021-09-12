f24b96e1865be704073ca6e4a795b6fdf00a5b1a
kameoman <spiderman.935963@gmail.com>
2021-09-12 16:55:46 +0900

    3at

--- main.py
+++ main.py
@@ -3,23 +3,57 @@ import urllib3
 from bs4 import BeautifulSoup
 import requests
 
-url = "http://usjinfo.com/attrWait.php?attr_id=2"
-
 st.title('待ち時間アプリ')
-http = urllib3.PoolManager()
-response = http.request("GET",url)
+if st.checkbox("ジュラシックパーク待ち時間"):
+  url = "http://usjinfo.com/attrWait.php?attr_id=2"
+  http = urllib3.PoolManager()
+  response = http.request("GET",url)
+  data = BeautifulSoup(response.data, "lxml")
+  title = data.title.string
+  t_title = (title.split("|")[0])
+  st.write(t_title)
+  data.find_all("div", attrs={"class": "realtime_status"})
+  w_time = data.find_all("div", attrs={"class": "realtime_status"})[0].string
+  w_time_int = int(w_time.split("分")[0])
+  w_time_str = str(w_time_int)
+  nowtime = data.find_all("h2")[1].string
+  n_time = (nowtime.split(">|<")[0])
+  st.write(n_time)
+  # st.write(w_time_int)
+  st.write(w_time_str+"分")
 
-if response.status == 200:
-  print("正常通信")
+if st.checkbox("NEW アメージング・アドベンチャー・オブ・スパイダーマン(TM)・ザ・ライド 4K3D"):
+  url = "http://usjinfo.com/attrWait.php?attr_id=18"
+  http = urllib3.PoolManager()
+  response = http.request("GET",url)
+  data = BeautifulSoup(response.data, "lxml")
+  title = data.title.string
+  t_title = (title.split("|")[0])
+  st.write(t_title)
+  data.find_all("div", attrs={"class": "realtime_status"})
+  w_time = data.find_all("div", attrs={"class": "realtime_status"})[0].string
+  w_time_int = int(w_time.split("分")[0])
+  w_time_str = str(w_time_int)
+  nowtime = data.find_all("h2")[1].string
+  n_time = (nowtime.split(">|<")[0])
+  st.write(n_time)
+  # st.write(w_time_int)
+  st.write(w_time_str+"分")
 
-data = BeautifulSoup(response.data, "lxml")
-title = data.title.string
-t_title = (title.split("|")[0])
-print(t_title)
-data.find_all("div", attrs={"class": "realtime_status"})
-w_time = data.find_all("div", attrs={"class": "realtime_status"})[0].string
-w_time_int = int(w_time.split("分")[0])
-nowtime = data.find_all("h2")[1].string
-n_time = (nowtime.split(">|<")[0])
-print(n_time)
-print(w_time_int)
+if st.checkbox("ハリウッド・ドリーム・ザ・ライド"):
+  url = "http://usjinfo.com/attrWait.php?attr_id=8"
+  http = urllib3.PoolManager()
+  response = http.request("GET",url)
+  data = BeautifulSoup(response.data, "lxml")
+  title = data.title.string
+  t_title = (title.split("|")[0])
+  st.write(t_title)
+  data.find_all("div", attrs={"class": "realtime_status"})
+  w_time = data.find_all("div", attrs={"class": "realtime_status"})[0].string
+  w_time_int = int(w_time.split("分")[0])
+  w_time_str = str(w_time_int)
+  nowtime = data.find_all("h2")[1].string
+  n_time = (nowtime.split(">|<")[0])
+  st.write(n_time)
+  # st.write(w_time_int)
+  st.write(w_time_str+"分")
