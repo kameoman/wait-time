b9eceb0dab72c421133ccaaa3b62600253c69db1
kameoman <spiderman.935963@gmail.com>
2021-09-24 22:46:24 +0900

    main

--- main.py
+++ main.py
@@ -183,6 +183,23 @@ if button2:
         st.write(n_time1+"時点")
         st.write(w_time_str+"分")
 
+# USJのアトラクション待ち時間抽出オブジェクト
+def time_extra():
+    http = urllib3.PoolManager()
+    response = http.request("GET",url)
+    data = BeautifulSoup(response.data, "lxml")
+    # アトラクション名（スクレイピング必要時）
+    # title = data.title.string
+    # t_title = (title.split("|")[0])
+    # st.write(t_title)
+    data.find_all("div", attrs={"class": "realtime_status"})
+    w_time = data.find_all("div", attrs={"class": "realtime_status"})[0].string
+    w_time_int = int(w_time.split("分")[0])
+    w_time_str = str(w_time_int)
+    nowtime = data.find_all("h2")[1].string
+    n_time = (nowtime.split(">|<")[0])
+    st.write(n_time)
+    st.write(w_time_str+"分")
 
 if button3:
   url = "https://usjinfo.com/"
@@ -200,48 +217,15 @@ if button3:
   st.write("営業時間"+open_time1+"～"+open_time2)
   if st.checkbox("ジュラシックパーク待ち時間"):
     img = Image.open("usj-jurassic-park-the-ride-logo.png")
-    img2 = Image.open("usj-jurassic-park-the-ride-long-necked-dinosaur-c.jpg")
-    # img3 = Image.open("usj-jurassic-park-the-ride-logo.png")
     st.image(img)
     url = "http://usjinfo.com/attrWait.php?attr_id=2"
-    http = urllib3.PoolManager()
-    response = http.request("GET",url)
-    data = BeautifulSoup(response.data, "lxml")
-    title = data.title.string
-    t_title = (title.split("|")[0])
-    st.write(t_title)
-    data.find_all("div", attrs={"class": "realtime_status"})
-    w_time = data.find_all("div", attrs={"class": "realtime_status"})[0].string
-    w_time_int = int(w_time.split("分")[0])
-    w_time_str = str(w_time_int)
-    nowtime = data.find_all("h2")[1].string
-    n_time = (nowtime.split(">|<")[0])
-    st.write(n_time)
-    # st.write(w_time_int)
-    st.write(w_time_str+"分")
-    st.image(img2)
+    time_extra()
 
   if st.checkbox("NEW アメージング・アドベンチャー・オブ・スパイダーマン(TM)・ザ・ライド 4K3D"):
     img = Image.open("usj-the-amazing-adventures-of-spider-man-the-ride-logo.png")
-    img2 = Image.open("usj-the-amazing-adventures-of-spider-man-the-ride-water-c.jpg")
     st.image(img)
     url = "http://usjinfo.com/attrWait.php?attr_id=18"
-    http = urllib3.PoolManager()
-    response = http.request("GET",url)
-    data = BeautifulSoup(response.data, "lxml")
-    title = data.title.string
-    t_title = (title.split("|")[0])
-    st.write(t_title)
-    data.find_all("div", attrs={"class": "realtime_status"})
-    w_time = data.find_all("div", attrs={"class": "realtime_status"})[0].string
-    w_time_int = int(w_time.split("分")[0])
-    w_time_str = str(w_time_int)
-    nowtime = data.find_all("h2")[1].string
-    n_time = (nowtime.split(">|<")[0])
-    st.write(n_time)
-    # st.write(w_time_int)
-    st.write(w_time_str+"分")
-    st.image(img2)
+    time_extra()
 
   if st.checkbox("ハリウッド・ドリーム・ザ・ライド"):
     img = Image.open("usj-hollywood-dream-the-ride-logo.png")
